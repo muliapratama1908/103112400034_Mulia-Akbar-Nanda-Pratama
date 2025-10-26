da81716f1a794f877bfa452cad8949fafdc27ea3
muliapratama1908 <muliapratama603@gmail.com>
2025-10-26 22:42:58 +0700

    Delete week 5 directory

--- week 5/guided 2/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-void append(Node* &head, int value){
-    Node *newNode = new Node{value, nullptr};
-    if(!head) head = newNode;
-    else{
-        Node *temp = head;
-        while(temp->next) temp = temp->next;
-        temp->next = newNode;
-    }
-}
